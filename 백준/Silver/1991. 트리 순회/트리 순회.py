126afcd76f21be071b963f9ca6b696ede818478a
yoahn <46748334+AYoungSn@users.noreply.github.com>
2025-03-19 17:21:30 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 34308 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,39 @@
+import sys
+from collections import defaultdict
+
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+
+tree = defaultdict(list)
+
+for i in range(N):
+    node, left, right = input().split()
+    tree[node] = [left, right]
+
+def preorder(root):
+    print(root, end='')
+    if tree[root][0] != '.':
+        preorder(tree[root][0])
+    if tree[root][1] != '.':
+        preorder(tree[root][1])
+
+def inorder(root):
+    if tree[root][0] != '.':
+        inorder(tree[root][0])
+    print(root, end='')
+    if tree[root][1] != '.':
+        inorder(tree[root][1])
+
+def postorder(root):
+    if tree[root][0] != '.':
+        postorder(tree[root][0])
+    if tree[root][1] != '.':
+        postorder(tree[root][1])
+    print(root, end='')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
