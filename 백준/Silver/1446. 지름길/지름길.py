7f39d52fb2e7daf97c0bcdaa7c896ebb153e8c58
yoahn <46748334+AYoungSn@users.noreply.github.com>
2025-03-20 16:11:15 +0900

    [Silver I] Title: 지름길, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,21 +1,17 @@
 import sys
-
 input = lambda: sys.stdin.readline().rstrip()
 N, D = map(int, input().split())
 graph = []
 dist = [i for i in range(D + 1)]
 for _ in range(N):
   s, e, w = map(int, input().split())
-  if e - s > w:
+  if e - s > w and e <= D:
     graph.append((s, e, w))
-
 graph.sort()
 
 for s, e, w in graph:
-  for i in range(1, D + 1):
-    if i == e: # s -> end == i
-      dist[i] = min(dist[i], dist[s] + w)
-    else:
-      dist[i] = min(dist[i], dist[i - 1] + 1)
+  dist[e] = min(dist[e], dist[s] + w)
+  for i in range(e, D + 1):
+    dist[i] = min(dist[i], dist[i - 1] + 1)
 
 print(dist[D])
