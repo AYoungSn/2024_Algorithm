e4550fd522291b89b72550e6e7d15ce0c0d0956c
yoahn <46748334+AYoungSn@users.noreply.github.com>
2025-05-14 18:45:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.76 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,15 +3,26 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+//         Arrays.sort(participant);
+//         Arrays.sort(completion);
         
+//         for (int i = 0; i < completion.length;i++) {
+//             if (!participant[i].equals(completion[i])) {
+//                 return participant[i];
+//             }
+//         }
+        
+//         return participant[participant.length - 1];
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < completion.length;i++) {
-            if (!participant[i].equals(completion[i])) {
+            map.put(completion[i], map.getOrDefault(completion[i], 0) + 1);
+        }
+        for (int i = 0; i < participant.length; i++) {
+            if (map.getOrDefault(participant[i], 0) == 0) {
                 return participant[i];
             }
+            map.put(participant[i], map.getOrDefault(participant[i], 1) - 1);
         }
-        
-        return participant[participant.length - 1];
+        return answer;
     }
 }
