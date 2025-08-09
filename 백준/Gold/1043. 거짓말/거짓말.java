8a7e71dc56f5e20e2af58da5ad04b81d89767ae7
yoahn <46748334+AYoungSn@users.noreply.github.com>
2025-08-09 15:52:41 +0900

    [Gold IV] Title: 거짓말, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,93 +1,81 @@
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int[] parent;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());//person
+		int M = Integer.parseInt(st.nextToken());//party
+		st = new StringTokenizer(br.readLine());
+		int cnt = Integer.parseInt(st.nextToken());
+		parent = new int[N + 1];
+		if (cnt == 0) {
+			System.out.println(M);
+			return;
+		}
+		for (int i = 0; i < N + 1; i++) {
+			parent[i] = i;
+		}
+		boolean[] knows = new boolean[N + 1];
+		for (int i = 0; i < cnt; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			knows[a] = true;
+		}
+		List<Integer>[] parties = new List[M];
+		String[] inputs;
+		for (int i = 0; i < M; i++) {
+			parties[i] = new ArrayList<>();
+			inputs = br.readLine().split(" ");
+			int num = Integer.parseInt(inputs[0]);
+			if (num > 0) {
+				parties[i].add(Integer.parseInt(inputs[1]));
+			}
+			for (int j = 1; j < num; j++) {
+				int a = Integer.parseInt(inputs[j]);
+				int b = Integer.parseInt(inputs[j + 1]);
+				union(a, b);
+				parties[i].add(b);
+			}
+		}
+		// 진실을 아는 사람과 같은 파티 참석한 경우 아는 사람으로 체크
+		for (int i = 1; i < N + 1; i++) {
+			if (knows[i]) {
+				int root = find(i);
+				for (int j = 1; j < N + 1; j++) {
+					if (root == find(j)) {
+						knows[j] = true;
+					}
+				}
+			}
+		}
+		int answer = M;
+		for (int m = 0; m < M; m++) {
+			for (int p: parties[m]) {
+				if (knows[p]) {
+					answer--;
+					break;
+				}
+			}
+		}
+		System.out.println(answer);
+	}
 
-    static int N, M; // N: 사람, M: 파티
-    static int[] parent;
-    static boolean[] people_know;
-    static List<Integer>[] parties;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        parent = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
-        }
-        people_know = new boolean[N + 1];
-        st = new StringTokenizer(br.readLine());
-        int cnt = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < cnt; i++) {
-            people_know[Integer.parseInt(st.nextToken())] = true;
-        }
-        String[] inputs;
-        parties = new ArrayList[M + 1];
-        // 같은 파티에 오는 사람들끼리 연관관계 이어줌
-        for (int i = 1; i <= M; i++) {
-            parties[i] = new ArrayList<>();
-            inputs = br.readLine().split(" ");
-            int party_num = Integer.parseInt(inputs[0]);
-            if (party_num <= 1) {
-                parties[i].add(Integer.parseInt(inputs[1]));
-            }
-            for (int j = 1; j < party_num; j++) {
-                int a = Integer.parseInt(inputs[j]);
-                int b = Integer.parseInt(inputs[j + 1]);
-                if (find(a) != find(b)) {
-                    union(a, b);
-                }
-                parties[i].add(b);
-            }
-            parties[i].add(Integer.parseInt(inputs[1]));
-        }
-        // 진실을 아는 사람들간의 연관관계가 있으면 -> 진실을 아는 사람으로 체크
-        boolean[] visited = new boolean[N + 1];
-        for (int i = 1; i <= N; i++) {
-            if (people_know[i] && !visited[i]) {
-                int root = find(i);
-                for (int j = 1; j <= N; j++) {
-                    if (root == find(j)) {
-                        people_know[j] = true;
-                        visited[j] = true;
-                    }
-                }
-            }
-        }
-        int result = 0;
-        // 진실을 아는 사람이 없어야 거짓 얘기 가능
-        for (int p = 1; p <= M; p++) {
-            boolean flag = true;
-            for (Integer attend :
-                    parties[p]) {
-                if (people_know[attend]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                result++;
-            }
-        }
-        System.out.println(result);
-    }
-
-    static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x <= y) {
-            parent[y] = x;
-        } else {
-            parent[x] = y;
-        }
-    }
-
-    static int find(int a) {
-        if (parent[a] == a) {
-            return parent[a];
-        }
-        return find(parent[a]);
-    }
+	static void union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+		if (a <= b) {
+			parent[pb] = pa;
+		} else {
+			parent[pa] = pb;
+		}
+	}
+	static int find(int a) {
+		if (parent[a] == a) {
+			return a;
+		}
+		return find(parent[a]);
+	}
 }
