c85478e02d53cea4f49611de6145890b6222c543
yoahn <46748334+AYoungSn@users.noreply.github.com>
2025-08-09 19:58:40 +0900

    [Gold IV] Title: 최단경로, Time: 828 ms, Memory: 111760 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,80 +1,53 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-class Node {
-    int end;
-    int weight;
-
-    public Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
-}
 public class Main {
-//    static int[][] graph;
-    static List<Node>[] graph;
-    static Queue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-        @Override
-        public int compare(int[] o1, int[] o2) {
-            return o1[1] - o2[1];
-        }
-    });
-//    static List<Integer>[] graph;
-    static int[] path;
-    static int V, E, start;
+	static class Node {
+		int end;
+		int weight;
+		Node(int e, int w) {
+			end = e;
+			weight = w;
+		}
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(br.readLine());
+		List<Node>[] graph = new List[V + 1];
+		int[] dist = new int[V + 1];
 
-    public static void main(String[] args) throws Exception{
-        // 다익스트라
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(br.readLine());
-//        graph = new int[V + 1][V + 1];
-        graph = new ArrayList[V + 1];
-        for (int i = 0; i < V + 1; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-//            graph[u][v] = w;
-            graph[u].add(new Node(v, w));
-        }
-
-        boolean[] vst = new boolean[V + 1];
-        path = new int[V + 1];
-        for (int i = 1; i < V + 1; i++) {
-            path[i] = Integer.MAX_VALUE;
-        }
-        path[start] = 0;
-        pq.add(new int[]{start, 0});
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            if (vst[curr[0]]) {
-                continue;
-            }
-//            System.out.println("curr: " + Arrays.toString(curr));
-            vst[curr[0]] = true;
-            path[curr[0]] = curr[1];
-            for (int i = 0; i < graph[curr[0]].size(); i++) {
-                if (path[graph[curr[0]].get(i).end] > path[curr[0]] + graph[curr[0]].get(i).weight) {
-                    path[graph[curr[0]].get(i).end] = path[curr[0]] + graph[curr[0]].get(i).weight;
-                    pq.add(new int[]{graph[curr[0]].get(i).end, curr[1] + graph[curr[0]].get(i).weight});
-                }
-            }
-        }
-        for (int i = 1; i < V+1; i++) {
-            if (i == start) {
-                System.out.println(0);
-            } else if (path[i] == Integer.MAX_VALUE) {
-                System.out.println("INF");
-            } else {
-                System.out.println(path[i]);
-            }
-        }
-    }
+		for (int i = 0; i < V + 1; i++) {
+			graph[i] = new ArrayList<>();
+			dist[i] = Integer.MAX_VALUE;
+		}
+		for (int e = 0; e < E; e++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			graph[u].add(new Node(v, w));
+		}
+		Queue<int[]> que = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+		que.add(new int[]{K, 0});
+		dist[K] = 0;
+		while(!que.isEmpty()) {
+			int[] cur = que.poll();
+			for (Node node : graph[cur[0]]) {
+				if (dist[node.end] > cur[1] + node.weight) {
+					dist[node.end] = cur[1] + node.weight;
+					que.add(new int[]{node.end, dist[node.end]});
+				}
+			}
+		}
+		for (int i = 1; i <= V; i++) {
+			if (dist[i] == Integer.MAX_VALUE) {
+				System.out.println("INF");
+				continue;
+			}
+			System.out.println(dist[i]);
+		}
+	}
 }
