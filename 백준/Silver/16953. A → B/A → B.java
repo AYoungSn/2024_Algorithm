0949c2d37367c2dbd33b3a722b0b97426d6112e9
yoahn <46748334+AYoungSn@users.noreply.github.com>
2025-08-10 09:59:08 +0900

    [Silver II] Title: A → B, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,33 +1,30 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int init = Integer.parseInt(st.nextToken());
+		int target = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int res = 1;
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{B, 1});
-        while (true) {
-            int[] cur = que.poll();
-            if (cur[0] % 10 == 1) {
-                que.add(new int[]{cur[0] / 10, cur[1] + 1});
-            }
-            if (cur[0] % 2 == 0) {
-                que.add(new int[]{cur[0] / 2, cur[1] + 1});
-            }
-            if (cur[0] == A) {
-                System.out.println(cur[1]);
-                break;
-            }
-            if ((cur[0] % 10 != 1 && cur[0] % 2 != 0) || cur[0] < A){
-                System.out.println(-1);
-                break;
-            }
-        }
-    }
-
+		Queue<int[]> que = new LinkedList<>();
+		que.add(new int[]{target, 1});
+		while (!que.isEmpty()) {
+			int[] cur = que.poll();
+			if (cur[0] % 2 == 0) {
+				que.add(new int[]{cur[0] / 2, cur[1] + 1});
+			}
+			if (cur[0] % 10 == 1) {
+				que.add(new int[]{cur[0] / 10, cur[1] + 1});
+			}
+			if (cur[0] == init) {
+				System.out.println(cur[1]);
+				return;
+			}
+			if (cur[0] % 2 != 0 && cur[0] % 10 != 1 || cur[0] < init) {
+				System.out.println(-1);
+				break;
+			}
+		}
+	}
 }
